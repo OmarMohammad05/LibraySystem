be71c3093c797a518e3edbfafe20566880312046
OmarMohammad05 <omar2mohammad25@gmail.com>
2025-11-09 22:23:47 +0300

    Fixed ALLOWED_HOSTS for Render domain

--- libray/settings.py
+++ libray/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&@av@)u890q_l^)4t3qxwdwe-d=_ltalum@mw@u-aq5z)wjo8_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  ## update from  True
 
-ALLOWED_HOSTS = ['LibraySystem.com','127.0.0.1'] # updated this from '*'
+ALLOWED_HOSTS = ['LibraySystem..onrender.com','127.0.0.1'] # updated this from '*'
 
 
 # Application definition
